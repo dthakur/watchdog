cf859ee6439dcedf722bd9f6c91bf9b533de4b5a
Divya Thakur <rewire@gmail.com>
2019-07-21 17:22:46 -0700

    remove unused import

--- services/src/services/controller.ts
+++ services/src/services/controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body, Get, Delete, Param, Query } from '@nestjs/common';
 import Repository from '../repository';
 import { CreateServiceDto } from '../entities';
-import { IsPositive, IsNumber, Max, Min } from 'class-validator';
+import { IsNumber, Max, Min } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 class GetAllQuery {
